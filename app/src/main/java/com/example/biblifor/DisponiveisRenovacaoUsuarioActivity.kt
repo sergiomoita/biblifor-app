dbed49378ea9915ce389c1233e4ec57e63c7ec70
mlopescpu <mateuslopespinheiro2507@gmail.com>
2025-10-06 19:49:59 -0300

    primeira mudanca notebook

--- app/src/main/java/com/example/biblifor/DisponiveisRenovacaoUsuarioActivity.kt
+++ app/src/main/java/com/example/biblifor/DisponiveisRenovacaoUsuarioActivity.kt
@@ -1,6 +1,9 @@
 package com.example.biblifor
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,14 @@ class DisponiveisRenovacaoUsuarioActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val imagemLivro = findViewById<ImageView>(R.id.lopesRomeuJulietaTeste)
+        imagemLivro.setOnClickListener {
+
+            val navegarLivrosRenovaveis = Intent(this, RenovacaoEmprestimoUsuarioActivity::class.java)
+            startActivity(navegarLivrosRenovaveis)
+
+        }
+
     }
 }
